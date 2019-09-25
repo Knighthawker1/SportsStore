8904f5374f5b2133fd4133fad40302c3a518920c
John Young <johnfyoung22@yahoo.com>
2019-09-25 14:23:57 -0500

    Finished Ch 9
    Added a mobile view, though when using the mobile view, the home button no longer works.

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using System.Linq;
+
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
@@ -14,12 +15,11 @@ namespace SportsStore.WebUI.Controllers
         public PartialViewResult Menu(string category = null)
         {
             ViewBag.SelectedCategory = category;
-
             IEnumerable<string> categories = repository.Products
-                .Select(x => x.Category)
-                .Distinct()
-                .OrderBy(x => x);
-        return PartialView(categories);
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x);
+            return PartialView("FlexMenu", categories);
         }
     }
 }
